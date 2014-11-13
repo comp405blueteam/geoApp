b2141ce34f7e2a4dd6f87b3817f0804321e06294
Jeffrey Markley <jmarkley@lhup.edu>
2014-11-13 00:30:39 -0500

    Full analysis implemented

--- full_analysis.php
+++ full_analysis.php
@@ -5,6 +5,9 @@
     require_once("../constants.php");
     require_once("functions/gui.php");
     require_once("../geoApp/functions/db.php");
+    require_once("functions/analysis.php");
+    require_once("functions/utils.php");
+	
     $db = Db::getDbInstance();
     
     $chemicals = array();
@@ -25,6 +28,63 @@
     ";
 
     $objects = $db->getRset($sql);
+	
+	// IF ajax query for adding item to analysis
+	if (isset($_POST['element']) & isset($_POST['object']) & isset($_POST['ppm']) & !isset($_POST['flag'])){
+		
+		// Sanitize values for
+		$element = trim(sanitize(cleanInput($_POST['element'])));
+		$object = trim(sanitize(cleanInput($_POST['object'])));
+		$PPM = trim(sanitize(cleanInput($_POST['ppm'])));
+		
+		// return values
+		echo json_encode(array("element" => $element, "object" => $object, "ppm" => $PPM));
+		
+		exit();
+	}
+        
+        // If ajax query for results
+        if (isset($_POST['element']) & isset($_POST['object']) & isset($_POST['ppm']) & isset($_POST['flag'])){
+            
+            $element = $_POST['element'];
+            $object = $_POST['object'];
+            $ppm = $_POST['ppm'];
+            $maxPPM;
+            
+            $sql =
+            "
+            SELECT contaminant.danger_level
+            FROM contaminant
+            JOIN object
+            USING ( object_id ) 
+            JOIN chemical
+            USING ( chemical_id ) 
+            WHERE chemical.chemical_name = chemical.chemical_name
+            AND chemical.chemical_name =  '$element'
+            AND object.object_name =  '$object'
+            ";
+            
+            $maxPPM = $db->getVal($sql);
+            
+            if ($ppm > $maxPPM){
+                echo "<tr style='color:red'>";
+                
+                
+            }
+            
+            if ($ppm <= $maxPPM){
+                echo "<tr style='color:lime'>";                
+            }
+            
+            echo "<td align='left'>".$object."</td>";
+            echo "<td align='left'>".$element."</td>";
+            echo "<td align='left'>".$ppm." PPM"."</td>";
+            echo "<td align='left'>".$maxPPM." PPM"."</td>";
+            echo "</tr>";
+            
+            exit();
+            
+        }
 ?>
 
 <?php
@@ -50,6 +110,116 @@
     closeHeader($title);
 
 ?>
+
+<script>
+
+// Variables to hold list of items to be analyzed
+var addElement = new Array;
+var addObject = new Array;
+var addPPM = new Array;
+var results = new Array;
+
+//Runs when user clicks add button
+function add(){
+    var element = document.getElementById("elementSelect").value;
+    var object = document.getElementById("objectSelect").value;
+    var ppm = document.getElementById("ppmInput").value;
+    var dataString = {element:element, object:object, ppm:ppm};
+
+    $.ajax({        
+                type: "POST",
+                url: <?php echo "'".BASE_URL."full_analysis.php'" ?>,
+                dataType: "json",
+                data: dataString,
+                cache: false,
+                success: function(results)
+                {                   
+				        // Add sanitized results to arrays
+					addElement[addElement.length] = results.element;
+					addObject[addObject.length] = results.object;
+					addPPM[addPPM.length] = results.ppm;
+					// var html holds the output for the addTextarea
+					var html = "<table width='100%'>";
+					html += "<tr>";
+					html += "<th align='left'>";
+                                        html += "<p>Item Type</p>";
+                                        html += "</th>";
+					html += "<th align='left'>";
+                                        html += "<p>Element</p>";
+                                        html += "</th>";
+					html += "<th align='left'>";
+                                        html += "<p>Sample's Contaminant Level</p>";
+                                        html += "</th>";
+					html += "</tr>";
+					
+					// Add all items in list to the output
+					for(var i = 0; i < addElement.length; i++){
+			
+						html += "<tr>";
+			
+						html += "<td align='left'>";
+						html += addObject[i];
+						html += "</td>";
+						
+						html += "<td align='left'>";
+						html += addElement[i];
+						html += "</td>";
+						
+						html += "<td align='left'>";
+						html += addPPM[i];
+						html += " PPM";
+						html += "</td>";
+			
+						html += "</tr>";
+					}
+					html += "</table>";
+					document.getElementById('addTextarea').innerHTML = html;
+                }
+            });
+		return;	
+}
+
+// Runs when user clicks analyze results button
+function analyze(){
+    var flag = 1;
+    
+    var html = "<table width='100%'>";
+    html += "<tr>";
+    html += "<th align='left'>";
+    html += "<p>Item Type</p>";
+    html += "</th>";
+    html += "<th align='left'>";
+    html += "<p>Element</p>";
+    html += "</th>";
+    html += "<th align='left'>";
+    html += "<p>Sample's Contaminant Level</p>";
+    html += "</th>";
+    html += "<th align='left'>";
+    html += "<p>Maximum Contaminant Level</p>";
+    html += "</th>";
+    html += "</tr>";
+
+    for(var i = 0; i < addElement.length; i++){
+        
+        var dataString = {element:addElement[i], object:addObject[i], ppm:addPPM[i], flag:flag};
+        
+        $.ajax({        
+                type: "POST",
+                url: <?php echo "'".BASE_URL."full_analysis.php'" ?>,
+                data: dataString,
+                cache: false,
+                async: false,
+                success: function(results)
+                {                    
+                    html += results;
+                }
+            });
+    }
+    html += "</table>";
+    document.getElementById('resultsTextarea').innerHTML = html;
+}
+
+</script>
    
     <div id="content">
         <form name="contentForm" id="contentForm">
@@ -67,16 +237,16 @@
     			 </select><br/><br/>
     			 PPM:<br/>
     			 <input name="ppmInput" id="ppmInput"/><br/><br/>
-    			 <button name="addButton">Add</button><br/><br/>
-    			 <textarea disabled name="addTextarea" id="addTextarea" style="resize:none; overflow-y:auto; overflow-x:auto; width:100%; height:30%;";>RESULTS - USERS CANNOT EDIT TEXTFIELD</textarea>
+    			 <button name="addButton" type="button" onclick="add()">Add</button><br/><br/>
+    			 <div disabled name="addTextarea" id="addTextarea" style="resize:none; overflow-y:auto; overflow-x:auto; width:100%; height:30%;">RESULTS - USERS CANNOT EDIT TEXTFIELD</div>
     			 <div id="itemContainer">
-    				    <button name="runAnalysisButton" id="runAnalysisButton">Run Analysis</button>
+    				    <button name="runAnalysisButton" type="button" id="runAnalysisButton" onclick="analyze()">Run Analysis</button>
     			 </div>
     		  </div>
     	   </div>
     	   <div id="contentRightWindow">
     		  <div id="contentRightWindowContents">
-    		  	   <textarea disabled name="resultsTextarea" id="resultsTextarea" style="resize:none; overflow-y:auto; overflow-x:auto; width:100%; height:99%;";>RESULTS - USERS CANNOT EDIT TEXTFIELD</textarea>
+    		  	   <div disabled name="resultsTextarea" id="resultsTextarea" style="resize:none; overflow:auto; overflow-x:hidden; width:100%; height:99%;">RESULTS - USERS CANNOT EDIT TEXTFIELD</div>
     		  </div>
     	   </div>
         </form>
